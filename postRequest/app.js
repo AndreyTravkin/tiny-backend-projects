6529189c4699e7d2b05a57eabd77f580c073e97a
Andrei Travkin <andreytravkin5@gmail.com>
2020-10-10 21:04:11 -0500

    progress

--- postRequest/app.js
+++ postRequest/app.js
@@ -1,10 +1,23 @@
 let express = require('express');
 let app = express();
+let bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+let friends = ["Denis", "Ertem", "Yulia", "Andrei", "Nikita", "Kristina", "Vitaliy"];
  
 app.get('/', function (req, res) {
   res.render('home');
+})
+
+app.post('/addfriend', function(req, res) {
+    let newFriend = req.body.newFriend;
+    friends.push(newFriend);
+    res.redirect("/friends");
+})
+
+app.get('/friends', function(req, res){
+    res.render("friends", {friends: friends});
 })
  
 app.listen(process.env.PORT || 3000, process.env.IP, function() {
